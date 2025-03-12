f8e0c7a0ce034ba747ff5c8d7f811c3f64ade9f1
Tgaurav856 <thakregaurav999@gmail.com>
2025-03-12 16:07:20 +0530

    Removed config folder

--- config/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose=require("mongoose");
-const db=mongoose.connect( process.env.DB_URL)
-.then(()=>console.log("db connected"))
-.catch((error)=>console.log("db connection failed",error))
--- config/redis.js
+++
@@ -1,4 +0,0 @@
-const redis = require('redis');
-const client = redis.createClient({ url: process.env.REDIS_URL });
-client.connect();
-module.exports = client;
